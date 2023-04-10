c280e91f9319eb806d8dd3340a4f8d4233715bb8
AISELab <dahyun1q@naver.com>
2023-04-10 23:40:16 +0900

    window&scoreboard

--- src/main/java/spaceinvaders/Window.java
+++ src/main/java/spaceinvaders/Window.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Objects;
 import javax.swing.ImageIcon;
 
 public class Window extends JFrame {
@@ -26,53 +25,61 @@ public class Window extends JFrame {
             public void paintComponent(Graphics g) {
                 super.paintComponent(g);
                 // 이미지 파일 경로를 적절히 수정하세요.
-                ImageIcon icon = new ImageIcon(getClass().getResource("/sprites/Background.png"));
+                ImageIcon icon = new ImageIcon(getClass().getResource("/sprites/windowBackground.png"));
                 Image image = icon.getImage();
                 g.drawImage(image, 0, 0, getWidth(), getHeight(), null);
 
-                ImageIcon btImage = new ImageIcon(getClass().getResource("/sprites/gameModeButton.png"));
-                g.drawImage(btImage.getImage(), 23, 400, 750, 100, null);
+                ImageIcon btImage = new ImageIcon(getClass().getResource("/sprites/windowButtons.png"));
+                g.drawImage(btImage.getImage(), 59, 30, 690, 510, null);
 
-                ImageIcon myPageButton = new ImageIcon(getClass().getResource("/sprites/myPageButton.png"));
-                g.drawImage(myPageButton.getImage(), 700, 30, 50, 50, null);
+//                ImageIcon optionButton = new ImageIcon(getClass().getResource("/sprites/optionButton.png"));
+//                g.drawImage(optionButton.getImage(), 440,430,320,110, null);
             }
         };
 
         panel.setLayout(null);
 
-        JButton button1 = new JButton("mashupmode");
-        JButton button2 = new JButton("scoremode");
-        JButton button3 = new JButton("2P mode");
-        JButton myPageButton = new JButton();
+        JButton button1 = new JButton();
+        JButton button2 = new JButton();
+        JButton button3 = new JButton();
+        JButton optionButton = new JButton();
         JButton ranking = new JButton();
 
         //버튼 투명하게
         button1.setOpaque(false);
         button2.setOpaque(false);
         button3.setOpaque(false);
-        myPageButton.setOpaque(false);
+        optionButton.setOpaque(false);
+        ranking.setOpaque(false);
 
 
         //버튼 테투리 없애기
-        myPageButton.setBorderPainted(false);
+        optionButton.setBorderPainted(false);
+        button1.setBorderPainted(false);
+        button2.setBorderPainted(false);
+        button3.setBorderPainted(false);
+        ranking.setBorderPainted(false);
 
-        button1.setBounds(25, 400, 250, 100);
-        button2.setBounds(275, 400, 250, 100);
-        button3.setBounds(525, 400, 250, 100);
-        myPageButton.setBounds(700, 30, 50, 50);
+        button1.setBounds(59, 30, 320, 110);
+        button2.setBounds(59, 160, 320, 110);
+        button3.setBounds(59, 300, 320, 110);
+        optionButton.setBounds(440, 430, 320, 110);
+        ranking.setBounds(59,430,320,110);
 
-        button1.setFont(new Font("Arial", Font.PLAIN, 20));
+        button1.setFont(new Font("Arial", Font.PLAIN, 60));
         button2.setFont(new Font("Arial", Font.PLAIN, 20));
         button3.setFont(new Font("Arial", Font.PLAIN, 20));
 
         button1.setBackground(new Color(36, 54, 105));
         button2.setBackground(new Color(36, 54, 105));
         button3.setBackground(new Color(36, 54, 105));
-        myPageButton.setBackground(new Color(36, 54, 105));
+        optionButton.setBackground(new Color(36, 54, 105));
+        ranking.setBackground(new Color(36,54, 105 ));
 
         button2.setForeground(Color.WHITE);
         button1.setForeground(Color.WHITE);
         button3.setForeground(Color.WHITE);
+        ranking.setForeground(Color.WHITE);
 
         button3.addActionListener(new ActionListener() {
 
@@ -92,7 +99,8 @@ public class Window extends JFrame {
         panel.add(button1);
         panel.add(button2);
         panel.add(button3);
-        panel.add(myPageButton);
+        panel.add(optionButton);
+        panel.add(ranking);
 
         getContentPane().add(panel);
 
